f5d0bdbd684bacbf3cfb054f5273bcd9f0d66a3d
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2020-10-01 18:01:24 +0530

    Updated Resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,10 +2,10 @@ import React from 'react';
 import right from '../img/arrow-right.svg';
 
 const resume = () => {
-
+	var RESUME = "https://drive.google.com/file/d/1G1G9dhEUF-hOYDlxM2WzKeeSMLCGGEHG/view?usp=sharing"
 	return (
 		<div class="resume">
-			<a href="https://bit.ly/jai-dewani" target="_blank" rel="noopener noreferrer">
+			<a href={RESUME} target="_blank" rel="noopener noreferrer">
 				View my resume <img  src={right} alt="right" />
 			</a>
 		</div>
