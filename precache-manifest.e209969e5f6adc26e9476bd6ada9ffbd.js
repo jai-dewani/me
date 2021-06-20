d1a186955651a52636c3a6a8303282d39d7e63ec
jai-dewani <jai-dewani@users.noreply.github.com>
2021-06-20 20:23:17 +0000

    deploy: b656c16e534cc7dd17dcea5d6eef8abd2b9d7762

---
+++ precache-manifest.e209969e5f6adc26e9476bd6ada9ffbd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d14e9fb9e8fed88ca3bfce4a9d7cd89",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "34593f50ffa7aab48548",
+    "url": "/me/static/css/main.ae28d0e0.chunk.css"
+  },
+  {
+    "revision": "b04bb234dca351effef9",
+    "url": "/me/static/js/2.cc8a8f9f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/me/static/js/2.cc8a8f9f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34593f50ffa7aab48548",
+    "url": "/me/static/js/main.050ad17a.chunk.js"
+  },
+  {
+    "revision": "9f017c6126d532f2efe6",
+    "url": "/me/static/js/runtime-main.c5199b1f.js"
+  },
+  {
+    "revision": "65143172d9f66f268362fee4851b9683",
+    "url": "/me/static/media/Jai-small.65143172.jpg"
+  },
+  {
+    "revision": "487c46655ea91c50b2a3c3c39c630060",
+    "url": "/me/static/media/arrow-right.487c4665.svg"
+  },
+  {
+    "revision": "7887c80e8534a0d3e1a52f1b26f5d21c",
+    "url": "/me/static/media/codechef.7887c80e.svg"
+  },
+  {
+    "revision": "70ad20d6d78bbbfd9c9254f8870b7450",
+    "url": "/me/static/media/codeforces.70ad20d6.svg"
+  },
+  {
+    "revision": "39c094f9f51fb898078a09cdea6e05b4",
+    "url": "/me/static/media/envelope.39c094f9.svg"
+  },
+  {
+    "revision": "f5a069d24c7855d24188c14f570b6a57",
+    "url": "/me/static/media/github.f5a069d2.svg"
+  },
+  {
+    "revision": "6f7f616f44e4c4244580338661f876d9",
+    "url": "/me/static/media/instagram.6f7f616f.svg"
+  },
+  {
+    "revision": "22bd17393aa39a4cfec33c206db5f0df",
+    "url": "/me/static/media/linkedin-in.22bd1739.svg"
+  },
+  {
+    "revision": "0729c42ac6c88ea12311c6989df785d6",
+    "url": "/me/static/media/twitter.0729c42a.svg"
+  },
+  {
+    "revision": "60d84743cda3de80d09fd71207fabb08",
+    "url": "/me/static/media/wave.60d84743.gif"
+  }
+]);
